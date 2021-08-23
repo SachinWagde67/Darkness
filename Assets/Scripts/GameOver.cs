8890e1a8f649032192b686e17d8bf3b25510211f
SACHIN <sachinwagde67@gmail.com>
2021-08-23 22:40:29 +0530

    GmeOver Canvas added

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitBtn()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField] private GameObject s1;
+    [SerializeField] private GameObject s2;
+    [SerializeField] private GameObject movingPlatform;
+
+    private void Start()
+    {
+        s1.SetActive(true);
+        s2.SetActive(false);
+        movingPlatform.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("sword"))
+        {
+            s1.SetActive(false);
+            s2.SetActive(true);
+            movingPlatform.SetActive(true);
+        }
+    }
+}
