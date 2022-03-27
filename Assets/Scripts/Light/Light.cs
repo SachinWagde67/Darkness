1a33c6bfc86aab590648b50cc452a7ceafc20605
Sachin Wagde <sachinwagde67@gmail.com>
2022-03-27 16:48:54 +0530

    no message

---
+++ Assets/Scripts/Light/Light.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private CharacterController2D player;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.GetComponent<CharacterController2D>() != null)
+        {
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            Vector2 rayDirection = (other.transform.position - transform.position);
+            RaycastHit2D hitlight = Physics2D.Raycast(transform.position, rayDirection, distance, playerLayer);
+            
+            if (hitlight.collider != null && hitlight.collider.GetComponent<CharacterController2D>() != null)
+            {
+                player.Death();
+            }
+        }
+    }
+}
